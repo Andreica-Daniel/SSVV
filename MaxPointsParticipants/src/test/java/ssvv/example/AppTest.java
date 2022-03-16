4d06aeabbd218440811a89e90107a8e84a0f7e34
Septimiu-Calin Bodica <tbodica@gmail.com>
2022-03-16 10:04:10 +0200

    Testing skeleton. Validator updated. #bugfix BVA. EC. Various problems.

--- MaxPointsParticipants/src/test/java/ssvv/example/AppTest.java
+++ MaxPointsParticipants/src/test/java/ssvv/example/AppTest.java
@@ -18,6 +18,9 @@ import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.Validator;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
 /**
  * Unit test for simple App.
  */
@@ -46,7 +49,7 @@ public class AppTest
         Validator<Nota> validatorNota = new NotaValidator();
         Validator<Tema> validatorTema = new TemaValidator();
         StudentXMLRepository studentXMLRepository = new StudentXMLRepository(validatorStudent, studentFileName);
-        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(validatorNota, notaFileName);
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(validatorNota, notaFileName); // a problem if file does not exist. todo: add checks
         TemaXMLRepository temaXMLRepository = new TemaXMLRepository(validatorTema, temaFileName);
 
         Service service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
@@ -65,6 +68,76 @@ public class AppTest
             // if student can't be deleted, it wasn't added. ^
             assertFalse(true);
         }
+    }
+
+    // todo: pre and post conditions: assert add + delete is effective before running the rest
+    // todo: move in other test class (new TC)
+    // todo: create fixtures.
+    // equivalence class: tests constructor() of Student.class
+    @Test
+    public void ec1() {
+
+        // todo: elevate to fixture.
+        ArrayList<Integer> invalidStreamStudentId = new ArrayList<>();
+        ArrayList<Integer> validStreamStudentId = new ArrayList<>();
+
+        ArrayList<Integer> validStreamStudentGroup = new ArrayList<>();
+        ArrayList<Integer> invalidStreamStudentGroup = new ArrayList<>();
+
+        invalidStreamStudentGroup.add(1);
+        validStreamStudentGroup.add(511);
+
+
+        invalidStreamStudentId.add(-1); // add values to test
+        invalidStreamStudentId.add(0);
+        validStreamStudentId.add(1);
+
+        Predicate<Integer> testStudentConstructorPredicate1 = (arg1) -> {
+            try {
+                new Student(arg1.toString(), "a", 500);
+                Validator<Student> studentValidator = new StudentValidator();
+                studentValidator.validate(new Student(arg1.toString(), "a", 500));
+            } catch(Exception exception)  {
+                return false;
+            }
+
+            return true;
+        };
+
+        // todo: merge args? optional args? specify? (refactor)
+        Predicate<Integer> testStudentConstructorPredicate2 = (arg1) -> {
+            try {
+                new Student("13", "a", arg1);
+                Validator<Student> studentValidator = new StudentValidator();
+                studentValidator.validate(new Student("13", "a", arg1));
+            } catch(Exception exception)  {
+                return false;
+            }
+            return true;
+        };
+
+        invalidStreamStudentId.forEach((Integer e) -> {
+            assertFalse(testStudentConstructorPredicate1.test(e));
+        });
+
+        validStreamStudentId.forEach((Integer e) -> {
+            assertTrue(testStudentConstructorPredicate1.test(e));
+        });
+
+    }
+
+    @Test
+    public void testStudentConstructor() {
+
+    }
+
+    @Test
+    public void testSaveStudent() {
+
+    }
+    @Test
+    public void tc_3() {
+
     }
 
 }
