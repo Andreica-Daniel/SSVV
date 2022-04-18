f6c3fa53ee964e0d7f549856a3cb842e536a2d7c
Septimiu-Calin Bodica <tbodica@gmail.com>
2022-04-18 07:55:46 +0300

    Refactor fixture.

--- src/test/java/ssvv/example/IntegrationTest.java
+++ src/test/java/ssvv/example/IntegrationTest.java
@@ -1,5 +1,7 @@
 package ssvv.example;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
@@ -12,6 +14,27 @@ public class IntegrationTest {
     public GradeTest gT = new GradeTest();
     public StudentTest sT = new StudentTest();
     public AssignmentsTest aT = new AssignmentsTest();
+
+    @BeforeEach
+    public void initFields() throws Exception {
+        this.gT = new GradeTest();
+        this.sT = new StudentTest();
+        this.aT = new AssignmentsTest();
+
+        this.gT.initializeArrays();
+        this.sT.initializeArrays();
+        this.aT.initializeArrays();
+
+        this.gT.SetUpXmlRepo();
+        this.sT.SetUpXmlRepo();
+    }
+
+    @AfterEach
+    public void cleanupFiles() throws Exception {
+        this.sT.CleanUpXmlRepo();
+        this.gT.CleanUpXmlRepo();
+        this.aT.cleanupGeneratedFiles();
+    }
 
     @Test
     public void test_1() {
@@ -29,18 +52,8 @@ public class IntegrationTest {
 
     @Test
     public void test_all() throws Exception {
-        gT.initializeArrays();
-        sT.initializeArrays();
-        aT.initializeArrays();
-
-        gT.SetUpXmlRepo();
         gT.test_ec1_addGrade_Stream_Id_Valid();
-        gT.CleanUpXmlRepo();
-        sT.SetUpXmlRepo();
         sT.test_ec1_addStudent_Stream_Group_Valid();
-        sT.CleanUpXmlRepo();
-
         aT.WhiteBoxTest1_Valid();
-        aT.cleanupGeneratedFiles();
     }
 }
